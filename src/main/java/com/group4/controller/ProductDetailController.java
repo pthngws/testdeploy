bdbad35b2f48f6a4ab7146bf0f67b2d26145e62b
Thành Lưu Quốc <22110415@student.hcmute.edu.vn>
2024-12-02 15:00:10 +0700

    ThanhLuu Push

--- src/main/java/com/group4/controller/ProductDetailController.java
+++ src/main/java/com/group4/controller/ProductDetailController.java
@@ -8,7 +8,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.group4.entity.InventoryEntity;
 import com.group4.entity.ProductEntity;
+import com.group4.service.IInventoryService;
 import com.group4.service.IProductDetailService;
 import com.group4.service.IProductService;
 
@@ -22,6 +24,9 @@ public class ProductDetailController {
 	@Autowired
 	private IProductDetailService productDetailService;
 	
+	@Autowired
+	private IInventoryService inventoryService; 
+	
 	
 	@GetMapping("/product/{id}")
     public String getProductDetail(Model model, @PathVariable Long id) {
@@ -34,7 +39,19 @@ public class ProductDetailController {
         	
         	
             model.addAttribute("product", productOptional.get());
+           
+            
+            //System.out.println(productOptional.get().getStatus());
             model.addAttribute("reviewCount", reviewCount);
+            
+            
+         // Nếu product có status = 1, lấy quantity từ InventoryEntity
+            if (productOptional.get().getStatus() == 1) {
+                Optional<InventoryEntity> inventoryOptional = inventoryService.findById(productOptional.get().getProductID());
+                inventoryOptional.ifPresent(inventory -> model.addAttribute("quantity", inventory.getQuantity()));
+            } else {
+                model.addAttribute("quantity", null); // Không có số lượng
+            }
             return "productDetails";  // Tên file HTML sẽ là product-details.html
         } else {
             // Nếu không tìm thấy sản phẩm, có thể chuyển đến trang lỗi hoặc trang không tìm thấy
