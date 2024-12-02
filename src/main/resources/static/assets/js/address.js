3748cb746d13aa03a98b637558d474550ca74109
truonggiang994 <157663315+truonggiang994@users.noreply.github.com>
2024-12-02 23:24:40 +0700

    push

--- src/main/resources/static/assets/js/address.js
+++ src/main/resources/static/assets/js/address.js
@@ -6,23 +6,23 @@ async function fetchAddressData() {
     try {
         const response = await fetch(addressFilePath);
         if (!response.ok) {
-            throw new Error("HTTP error! Status: ${response.status}");
+            throw new Error(`HTTP error! Status: ${response.status}`);
         }
         const data = await response.json();
-        populateCities(data);
+        populateProvinces(data);
     } catch (error) {
         console.error("Error fetching address data:", error);
     }
 }
 
 // Hàm hiển thị danh sách tỉnh/thành phố
-function populateCities(data) {
-    const citySelect = document.getElementById("city");
-    for (const city in data) {
+function populateProvinces(data) {
+    const provinceSelect = document.getElementById("province");
+    for (const province in data) {
         const option = document.createElement("option");
-        option.value = city;
-        option.textContent = city;
-        citySelect.appendChild(option);
+        option.value = province;
+        option.textContent = province;
+        provinceSelect.appendChild(option);
     }
     // Lưu dữ liệu vào một biến toàn cục để sử dụng cho danh sách quận/huyện
     window.addressData = data;
@@ -30,15 +30,15 @@ function populateCities(data) {
 
 // Hàm cập nhật danh sách quận/huyện dựa trên tỉnh/thành phố được chọn
 function updateDistricts() {
-    const citySelect = document.getElementById("city");
+    const provinceSelect = document.getElementById("province");
     const districtSelect = document.getElementById("district");
 
     // Xóa các quận/huyện cũ
-    districtSelect.innerHTML = '<option value="">Chọn Quận/Huyện</option>';
+    districtSelect.innerHTML = '<option value="">Select District</option>';
 
-    const selectedCity = citySelect.value;
-    if (selectedCity && window.addressData[selectedCity]) {
-        window.addressData[selectedCity].forEach(district => {
+    const selectedProvince = provinceSelect.value;
+    if (selectedProvince && window.addressData[selectedProvince]) {
+        window.addressData[selectedProvince].forEach(district => {
             const option = document.createElement("option");
             option.value = district;
             option.textContent = district;
